ff6c258f85545d2c6fa192b0b451df9ccb396adc
Sploder-Saptarshi <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-04-04 18:34:38 +0530

    Update to fossnet (real)

--- src/pc/configfile.h
+++ src/pc/configfile.h
@@ -14,8 +14,8 @@
 #define MAX_SAVE_NAME_STRING 32
 
 #define DEFAULT_PORT 7777
-#define DEFAULT_COOPNET_IP "net.coop64.us"
-#define DEFAULT_COOPNET_PORT 34197
+#define DEFAULT_COOPNET_IP "45.142.177.10"
+#define DEFAULT_COOPNET_PORT 4288
 
 typedef struct {
     unsigned int x, y, w, h;
